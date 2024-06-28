6a875268e18989503dc887310dc049ec8259d47c
warda <wardabounab4@gmail.com>
2024-06-29 00:48:35 +0100

    feat(sort algorithme):add function insertionSort in js

---
+++ sorting.js
@@ -0,0 +1,15 @@
+function insertionSort(arr) {
+  let key, j;
+  for (let i = 1; i < arr.length; i++) {
+    key = arr[i];
+    j = i - 1;
+
+    while (j >= 0 && arr[j] > key) {
+      arr[j + 1] = arr[j];
+      j = j - 1;
+    }
+    arr[j + 1] = key;
+  }
+  return arr;
+}
+console.log(insertionSort([23, 1, 10, 5, 2]));
